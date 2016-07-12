70062956daea7c9b8b966c3af9f55e2c42636398
Mr.Jois <joissumanth@gmail.com>
2016-07-12 09:51:42 +0530

    Update Worker.java

--- com.databox.generator/Worker.java
+++ com.databox.generator/Worker.java
@@ -6,17 +6,26 @@
 package com.databox.generator;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
-
+/**
+ *
+ * @author Jois
+ */
 public class Worker {
     
     public static final String Prepared = "PreparedStatement";
     public static final String Normal = "NormalStatement"; 
     public static final String SelectNormal = "NormalSelectStatement";
+    public static final String SelectWhere = "SelectWithWhere";
     private Map values;
     private String action;
     private String TableName;
     private AssistantQueryGenerator assistant;
+    private String whereColumn = null;
+    private String whereValue = null;
+    private String operator = null;
     
      public Worker(AssistantQueryGenerator assistant,Map values,String action,String TableName){
          this.action = action;
@@ -24,6 +33,16 @@ public class Worker {
          this.values = values;
          this.TableName = TableName;
       }
+     
+     public Worker(AssistantQueryGenerator assistant,Map values,String action,String TableName,String whereColumn,String whereValue,String operator){
+         this.action = action;
+         this.assistant = assistant;
+         this.values = values;
+         this.TableName = TableName;
+         this.whereColumn = whereColumn;
+         this.whereValue = whereValue;
+         this.operator = operator;
+     }
    
        //get query for Insert 
        public String getInsertQuery(String statementType){
@@ -40,10 +59,24 @@ public class Worker {
        
        //get query for Select 
        public String getSelectQuery(String statementType){
+           String query = action + assistant.getHeaders(values,statementType) + " FROM " + TableName;
            
-           return action + assistant.getHeaders(values,statementType) + " FROM " + TableName;
+           if(statementType.equals(SelectWhere)){
+               query+=" WHERE "+whereColumn+" "+operator+" '"+whereValue+"'" ;
+             query =   query.replace("Select", "SELECT");
+           }
+           return query;
+       
        }
-              
+           
+     
          
+       public static Map toMap(Set<String> set){
+           Map selections = new TreeMap();
+            for(String tableHeaders:set){
+              selections.put(tableHeaders, null);
+            }
+           return selections;
+       }
        
 }
