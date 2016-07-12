516871db8ea671d32826005998b24ff37ff48381
Mr.Jois <joissumanth@gmail.com>
2016-07-12 09:46:59 +0530

    Update DataBase.java

--- com.databox/DataBase.java
+++ com.databox/DataBase.java
@@ -1,13 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.databox;
 
 import java.sql.ResultSet;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public interface DataBase {
     public boolean Insert(String TableName,Map values);
     public boolean Delete(String TableName,Map values);
     public boolean Update(String TableName,Map newValues,Map whereValues);
-    public ResultSet Select(String TableName,List<String> values);
+    public ResultSet Select(String TableName,Set<String> values);
 }
